f49cc4a180777381c9e02639f5f116cf40cdb7af
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-01 19:33:07 -0800

    WIP towards dynamic selectino of qtr navigation links

--- static/js/select_section.js
+++ static/js/select_section.js
@@ -1,1 +1,26 @@
-console.log("select_section.js");
+
+
+
+$( document ).ready(function() {
+    console.log("select_section.js");
+    var offering = localStorage.getItem("offering");
+    console.log("offering=" + offering);
+
+    $(".offerings-dropdown-item").click(
+	function(){
+	    console.log("offering was clicked");
+	    var elem = $(this);
+	    var text = elem.text();
+	    var url = elem.data("url");
+	    console.log("text: " + text);
+	    console.log("url: " + url);
+	    var offering = {
+		"text" : text,
+		"url" : url
+	    };
+	    localStorage.setItem("offering",
+				 JSON.stringify(offering));
+	}
+    );
+    
+});
